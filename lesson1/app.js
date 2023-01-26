9bfc1c77039e5c6117784c6442b419292fe68356
LizaLarchenko <llarcheko@mail.ru>
2023-01-26 19:11:29 +0300

    start events lesson

--- lesson1/app.js
+++
@@ -1,8 +0,0 @@
-let btn = document.getElementById("btn");
-let text = document.getElementById("hello");
-
-if (btn) {
-  btn.addEventListener("click", () => {
-    text.innerText = "Hello World!";
-  });
-}
--- lesson4/app.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelector(".btn");
-let text = document.querySelector(".text");
-
-if (btn) {
-  btn.addEventListener("click", () => {
-    text.style.color = "red";
-  });
-}
